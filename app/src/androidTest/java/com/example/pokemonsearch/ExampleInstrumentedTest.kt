ae0651bf405ef8915a7dedfc6709ea8ae971f293
Shahariar K. Bhuiyan <shahariar.b@gmail.com>
2021-08-10 11:15:43 +0200

    Changed some tests

--- app/src/androidTest/java/com/example/pokemonsearch/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/pokemonsearch/ExampleInstrumentedTest.kt
@@ -1,24 +1,35 @@
 package com.example.pokemonsearch
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class CounterTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.pokemonsearch", appContext.packageName)
+    fun counter_initially_zero() {
+        val text = composeTestRule.activity.getString(R.string.clicks, 0)
+        composeTestRule.onNodeWithText(text).assertExists()
+    }
+
+    @Test
+    fun clickButton_increments_counter() {
+        val incrementCounterText = composeTestRule.activity.getString(R.string.increment_counter)
+
+        composeTestRule.onNodeWithText(incrementCounterText).performClick()
+
+        val clicksText = composeTestRule.activity.getString(R.string.clicks, 1)
+
+        composeTestRule.onNodeWithText(clicksText).assertExists()
     }
 }
--- app/src/main/java/com/example/pokemonsearch/MainActivity.kt
+++ app/src/main/java/com/example/pokemonsearch/MainActivity.kt
@@ -3,11 +3,23 @@ package com.example.pokemonsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pokemonsearch.ui.theme.PokemonSearchTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +29,7 @@ class MainActivity : ComponentActivity() {
             PokemonSearchTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("Jetpack")
                 }
             }
         }
@@ -26,13 +38,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var counter by remember {
+        mutableStateOf(0)
+    }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = stringResource(id = R.string.welcome_text, name))
+        Button(modifier = Modifier.padding(16.dp), onClick = { counter++ }) {
+            Text(stringResource(R.string.increment_counter))
+        }
+        Text(text = stringResource(id = R.string.clicks, counter))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PokemonSearchTheme {
-        Greeting("Android")
+        Greeting("Jetpack")
     }
 }
