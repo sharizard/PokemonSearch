54f3dd4503c46cce84fbbce909785c3763cedb50
Shahariar K. Bhuiyan <shahariar.b@gmail.com>
2021-08-10 15:07:12 +0200

    Added new linter and formatter

--- app/src/androidTest/java/com/example/pokemonsearch/CounterTest.kt
+++ app/src/androidTest/java/com/example/pokemonsearch/CounterTest.kt
@@ -4,11 +4,9 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
 class CounterTest {
--- app/src/main/java/com/example/pokemonsearch/MainActivity.kt
+++ app/src/main/java/com/example/pokemonsearch/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.pokemonsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
